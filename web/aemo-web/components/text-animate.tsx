5ed70b0d7d2c0f658c6b1a899fe400a5d8fe6cc4
as2811-project <anandh.tedxchennai@gmail.com>
2025-04-01 21:31:14 +1100

    Fix imports

--- web/aemo-web/components/text-animate.tsx
+++ web/aemo-web/components/text-animate.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { FC, useEffect, useRef } from "react";
-import { HTMLMotionProps, motion, useAnimation, useInView } from "motion/react";
+import { FC, useRef } from "react";
+import { HTMLMotionProps, motion } from "motion/react";
 
 type AnimationType =
   | "fadeIn"
@@ -202,12 +202,9 @@ const TextAnimate: FC<Props> = ({
   //   });
 
   const ref = useRef(null);
-  const isInView = useInView(ref, { once: true });
 
   const letters = Array.from(text);
   const { container, child } = animationVariants[type];
-
-  const ctrls = useAnimation();
 
   //   useEffect(() => {
   //     if (isInView) {
